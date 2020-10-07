7b6c9a1cb96056ba3e8a307633a00936ec5070b9
likithaaa <likithad1@gmail.com>
2020-10-06 22:21:14 -0400

    Set up basic hooks to view array of list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([
+    'Take dog on a walk',
+    'Take trash out',
+    'Work out!!',
+  ]);
+
+  const [input, setInput] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello world!</h1>
+      <input />
+      <button>Add to list</button>
+
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
