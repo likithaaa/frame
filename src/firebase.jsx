6ade5a2e4cc1dc0471f980229d144cb3427f6d2b
likithaaa <likithad1@gmail.com>
2020-10-07 10:36:02 -0400

    Update firebase config

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,4 +1,6 @@
-const firebaseConfig = {
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
   apiKey: 'AIzaSyBESC3CNUTt1GfmxKJWId7_VL0E4u4uH7I',
   authDomain: 'frame-todo.firebaseapp.com',
   databaseURL: 'https://frame-todo.firebaseio.com',
@@ -7,4 +9,8 @@ const firebaseConfig = {
   messagingSenderId: '1076378341571',
   appId: '1:1076378341571:web:86fdb57d78195b41aa5276',
   measurementId: 'G-V9QESDXDRW',
-};
+});
+
+const db = firebaseApp.firestore();
+
+export { db };
